fa83726681945f96db0e23053e1d5eabea981012
ananasness <ananasness@gmail.com>
2018-03-14 01:24:14 +0300

    Move routes to app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 import os, pickle
 from pathlib import Path
 from script import process_video
@@ -29,6 +29,24 @@ labels = pickle.load(open('./static/rects.p', 'rb'))
 app = Flask(__name__)
 app.debug = True
 
-import index
+filename = '/static/res/{}.jpg'
+
+@app.route("/")
+def hello():
+    pictures = []
+    for i in range(1, len(labels)):
+        if ((labels[i - 1] == 'no cup here' and labels[i] != 'no cup here') or
+                (labels[i - 1] != 'no cup here' and labels[i] == 'no cup here')):
+            pictures.append((filename.format(i-1), labels[i-1], i-1))
+            pictures.append((filename.format(i), labels[i], i))
+
+    return render_template('index.html', pictures=pictures)
+
+
+@app.route("/all")
+def show_all():
+    pictures = [(filename.format(i), labels[i], i) for i in range(len(labels))]
+    return render_template('index.html', pictures=pictures)
+
 
 app.run()
